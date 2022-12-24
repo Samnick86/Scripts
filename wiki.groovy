79e9a019da05bfe712d4200609caf1a8a3235b06
Samnick86 <jurikj86@gmail.com>
2022-12-24 23:18:21 +0100

    wiki

---
+++ wiki.groovy
@@ -0,0 +1,21 @@
+## Makro: Zobrazit podřazené stránky
+
+{children:depth=1|sort=title|reverse=false}
+* [{page.title}]({page.url})
+{children}
+
+## Použití makra
+
+{Zobrazit podřazené stránky}
+
+################################################################
+
+## Makro: Zobrazit podřazené stránky
+
+{children:exclude=current|sort=title|reverse=false}
+* [{page.title}]({page.url})
+{children}
+
+## Použití makra
+
+{Zobrazit podřazené stránky}
