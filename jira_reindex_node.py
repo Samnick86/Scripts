38e7c1b6257ec2420c0f47fdfdde2516cb509ce0
Samnick86 <jurikj86@gmail.com>
2022-12-24 23:01:49 +0100

    jira reindex node

---
+++ jira_reindex_node.py
@@ -0,0 +1,41 @@
+################################
+#pip install requests
+#pip install tkinter
+################################
+
+import requests
+import tkinter as tk
+
+# Vytvoření dialogového okna
+root = tk.Tk()
+root.title("Zobrazení průběhu reindexace")
+
+# Přihlášení pomocí basic auth
+auth = requests.auth.HTTPBasicAuth('jmeno', 'heslo')
+
+url = "https://moje.adresa.nodu"
+
+# Odeslání POST požadavku
+response = requests.post('https://myurl.com/rest/api/2/reindex', auth=auth, data={'type': 'FOREGROUND'})
+
+
+# Získání taskId ze získaného JSONu
+taskId = response.json()['taskId']
+
+# Odeslání GET požadavku pro získání informací o průběhu
+response = requests.get(f'https://myurl.com/rest/api/2/reindex/progress?taskId={taskId}', auth=auth)
+
+# Získání informací o průběhu a success ze získaného JSONu
+currentProgress = response.json()['currentProgress']
+success = response.json()['success']
+
+# Vytvoření labelů pro zobrazení informací
+label1 = tk.Label(root, text=f"Průběh: {currentProgress}%")
+label2 = tk.Label(root, text=f"Úspěch: {success}")
+
+# Zobrazení labelů
+label1.pack()
+label2.pack()
+
+# Spuštění dialogového okna
+root.mainloop()
